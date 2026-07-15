8005f11d6a1ebcbc1a891237d4b41b2d4e494992
emiledev <68334724+emiledev@users.noreply.github.com>
2026-07-15 14:18:21 -0400

    Updated Python interpreter from V3.9 to V3.12 to work on match cases

--- Main.py
+++ Main.py
@@ -1509,3 +1509,49 @@
 #passing_grades = [grade for grade in grades if grade >= 60]
 
 #print(passing_grades)
+
+# -------- Match-Case Statements --------
+# Match-case statement (switch): An alternative to using many 'elif' statements
+#                                Execute some code if a value matches a 'case'
+#                                Benefits: cleaner and syntax is more readable
+
+#def day_of_week(day):
+#    if day == 1:
+#        return "It is Sunday"
+#    elif day == 2:
+#        return "It is Monday"
+#    elif day == 3:
+#        return "It is Tuesday"
+#    elif day == 4:
+#        return "It is Wednesday"
+#    elif day == 5:
+#        return "It is Thursday"
+#    elif day == 6:
+#        return "It is Friday"
+#    elif day == 7:
+#        return "It is Saturday"
+#    else:
+#        return "Not a valid day"
+
+#print(day_of_week(1))
+
+def day_of_week(day):
+    match day: # -------- use match and then the corresponding case (day) and then add cases --------
+        case 1:
+            return "It is Sunday"
+        case 2:
+            return "It is Monday"
+        case 3:
+            return "It is Tuesday"
+        case 4:
+            return "It is Wednesday"
+        case 5:
+            return "It is Thursday"
+        case 6:
+            return "It is Friday"
+        case 7:
+            return "It is Saturday"
+        case _: # -------- an _ in a match-case statement is called a wild card. This case functions as the else statement --------
+            return "Not a valid day"
+
+print(day_of_week(1))
